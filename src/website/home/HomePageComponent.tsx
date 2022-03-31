2d94a58c134ee0c1f479fa82a1e53783f572005b
Samuel Zheng <samuelzheng11@gmail.com>
2022-04-01 00:23:21 +1300

    add face picture, make picture smaller and round

--- src/website/home/HomePageComponent.tsx
+++ src/website/home/HomePageComponent.tsx
@@ -1,9 +1,11 @@
 import PanelComponent from '../../core/panel/PanelComponent';
 import TextComponent from '../../core/text/TextComponent';
+import './HomePageComponent.scss'
 
 function HomePageComponent() {
     return(
-        <div>
+        <div className='home-page'>
+            <img src="MyFace.jpg" className="profile-image"></img>
             <PanelComponent size="medium">
                 <TextComponent>Hello this is the home page welcome</TextComponent>
             </PanelComponent>
