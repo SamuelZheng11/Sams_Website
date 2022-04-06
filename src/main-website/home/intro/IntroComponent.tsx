9528a443f533f02b1adb1d178356cdb9c271f63d
Samuel Zheng <samuelzheng11@gmail.com>
2022-04-06 15:52:00 +1200

    Update photo

--- src/main-website/home/intro/IntroComponent.tsx
+++ src/main-website/home/intro/IntroComponent.tsx
@@ -5,7 +5,7 @@ import { introMessage, name, welcomeMessage } from "./IntroConstants";
 function IntroComponent() {
     return(
         <div className='intro'>
-            <img src="MyFace.jpg" className="profile-image"></img>
+            <img src="MyFace.png" className="profile-image"></img>
             <Typography variant="subtitle1" className='intro-message'>{introMessage}</Typography>
             <Typography variant="h2">{name}</Typography>
             <Typography variant="h5">{welcomeMessage}</Typography>
