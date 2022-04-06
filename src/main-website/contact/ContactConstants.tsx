2d074b30c0863e471df3bd3481aaa636d47b1b96
Samuel Zheng <samuelzheng11@gmail.com>
2022-04-06 18:11:27 +1200

    change email back to sam@samzheng

--- src/main-website/contact/ContactConstants.tsx
+++ src/main-website/contact/ContactConstants.tsx
@@ -1,7 +1,7 @@
 export const CONTACT_TITLE = 'Contact';
 
 export const FULL_NAME = 'Samuel Kevin Bei-An Yu-Cong Zheng';
-export const EMAIL = 'media@samuelzheng.com';
+export const EMAIL = 'sam@samuelzheng.com';
 export const GIT_HUB = '/SamuelZheng11';
 export const LINKED_IN = '/in/samuelkevinbeianyucong/';
 export const LOCATION = 'Sydney, Australia';
