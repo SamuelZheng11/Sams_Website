d38ff8cfb5b858af0015b0cf12aa0507dc50109e
Samuel Zheng <samuelzheng11@gmail.com>
2022-04-21 11:11:44 +1200

    Staging (#20)
    * hotfix: fix CREDITS wording
    
    * Hotfix: replace tab thumbnail icon
    
    * Add projects page (#13)
    
    * hotfix: fix grammar on projects optimal scheduler
    
    * Feature/add light and dark theming to website (#15)
    
    * Add dark theme, add them slider, need to fix header opacity
    
    * Fix header
    
    * Feature/AppBar fixes/updates (#16)
    
    * Make App Bar hide when scrolling
    
    * Fix scrolling on mobile for app header
    
    * Feature/link up website be (#19)
    
    * hook up backend and frontend
    
    * resolve chrome console warnings
    
    * add all settled call to website to set loaded to true when all get request promises resolved

--- src/main-website/Website.tsx
+++ src/main-website/Website.tsx
@@ -1,33 +1,74 @@
 import { createRef, useEffect } from 'react';
+import axios from 'axios';
+import { Subscription } from 'rxjs';
 
-import { useAppSelector } from './WebsiteHooks';
-import { EWebsitePages } from './WebsiteTypes';
+import { useAppDispatch, useAppSelector } from './WebsiteHooks';
+import { EWebsitePages, IBio } from './WebsiteTypes';
 import HeaderComponent from './header/HeaderComponent';
 import HomePageComponent from './home/HomePageComponent';
 import ContactComponent from './contact/ContactComponent';
 import EmploymentComponent from './employment/EmploymentComponent';
 import EducationComponent from './education/EducationComponent';
 import ProjectComponent from './project/ProjectComponent';
-
-import './Website.scss'
 import AboutComponent from './about/AboutComponent';
 import FooterComponent from './footer/FooterComponent';
+import { setBio, setEducations, setEmployments, setProjects, setWebsiteInfoLoaded } from './slice/WebsiteInformationSlice';
+import { IEmployment } from './employment/EmploymentTypes';
+import { IProject } from './project/ProjectTypes';
+import { IEducation } from './education/EducationTypes';
+
+import './Website.scss'
 
 function Website() {
+	const dispatch = useAppDispatch()
+	const subscriptions: Subscription[] = [];
 	const view = useAppSelector(state => state.navigation.view);
-	const theme = useAppSelector(state => state.navigation.theme);
+	const theme = useAppSelector(state => state.theme.theme);
 
 	const [websiteRef, homeRef, aboutRef, employmentRef, educationRef, projectRef, contactRef] = [createRef<HTMLDivElement>(), createRef<HTMLDivElement>(), createRef<HTMLDivElement>(), createRef<HTMLDivElement>(), createRef<HTMLDivElement>(), createRef<HTMLDivElement>(), createRef<HTMLDivElement>()];
 
 	useEffect(() => {
-		checkViewToScrollTo()
+		checkViewToScrollTo(view)
 	}, [view]);
 
 	useEffect(() => {
 		theme !== 'light' ? websiteRef.current?.classList.add('is_inverted') : websiteRef.current?.classList.remove('is_inverted')
 	}, [theme]);
 
-	const checkViewToScrollTo = () => {
+	useEffect(() => {
+		loadWebsiteInfo();
+	})
+
+	useEffect( () => () => subscriptions.forEach(subscription => subscription.unsubscribe()), [] );
+
+	const loadWebsiteInfo = () => {
+		const backendUri = 'https://ec2-52-63-58-171.ap-southeast-2.compute.amazonaws.com';
+		Promise.allSettled([
+			axios.get(`${backendUri}/Bio`)
+			.then(response => {
+				const bioResponse = response.data[0] as IBio
+				dispatch(setBio(bioResponse))
+			}), 
+			axios.get(`${backendUri}/Education`)
+			.then(response => {
+				const educationResponse = response.data as IEducation[]
+				dispatch(setEducations(educationResponse))
+			}), 
+			axios.get(`${backendUri}/Employment`)
+			.then(response => {
+				const employmentResponse = response.data as IEmployment[]
+				dispatch(setEmployments(employmentResponse))
+			}), 
+			axios.get(`${backendUri}/Project`).then(response => {
+				const projectResponse = response.data as IProject[]
+				dispatch(setProjects(projectResponse))
+			})
+		]).then(() => {
+			dispatch(setWebsiteInfoLoaded(true));
+		})
+	}
+
+	const checkViewToScrollTo = (view: EWebsitePages) => {
 		switch(view) {
 			case EWebsitePages.home: 
 				homeRef.current?.scrollIntoView({behavior: "smooth"});
