613629966a572cf110c98967f33274a10f5c87b3
Sam Zheng <samuelzheng11@gmail.com>
2022-04-21 12:33:20 +1200

    Revert "Revert "Staging (#20)""
    This reverts commit 1bbb95001d7b1ba3dc1fdf28c6ddf39419bf74d0.

--- src/main-website/contact/ContactComponent.tsx
+++ src/main-website/contact/ContactComponent.tsx
@@ -5,38 +5,51 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { Typography } from '@mui/material';
 
-import { CONTACT_TITLE, EMAIL, FULL_NAME, GIT_HUB, LINKED_IN, LOCATION } from './ContactConstants';
+import { useAppSelector } from '../WebsiteHooks';
+import { CONTACT_TITLE } from './ContactConstants';
 import Layout from '../../core/layout/LayoutComponent';
+import LoadingSpinnerComponent from '../../core/loading/LoadingSpinnerComponent';
 
 import './ContactComponent.scss'
 
 function ContactComponent() {
+    const contentLoaded = useAppSelector(state => state.information.websiteInfoLoaded);
+    const contact = useAppSelector(state => state.information.bio?.contact);
+
+    const getFullnameFromContact = () => {
+        return contact !== null ? `${contact?.givenNames.join(' ')} ${contact?.surname}` : '';
+    };
+
     return(
         <div className='contact'>
             <div className='contact-title-container'>
                 <Typography variant="h4">{CONTACT_TITLE}</Typography>
             </div>
 
-            <Layout orientation='horizontal'>
-                <PersonIcon></PersonIcon>
-                <Typography className='contact-detail-text'>{FULL_NAME}</Typography>
-            </Layout>
-            <Layout orientation='horizontal'>
-                <MailOutlineIcon></MailOutlineIcon>
-                <Typography className='contact-detail-text'>{EMAIL}</Typography>
-            </Layout>
-            <Layout orientation='horizontal'>
-                <LocationOnIcon></LocationOnIcon>
-                <Typography className='contact-detail-text'>{LOCATION}</Typography>
-            </Layout>
-            <Layout orientation='horizontal'>
-                <GitHubIcon></GitHubIcon>
-                <Typography className='contact-detail-text'>{GIT_HUB}</Typography>
-            </Layout>
-            <Layout orientation='horizontal'>
-                <LinkedInIcon></LinkedInIcon>
-                <Typography className='contact-detail-text'>{LINKED_IN}</Typography>
-            </Layout>
+            <LoadingSpinnerComponent loaded={contentLoaded}>
+                <div>
+                    <Layout orientation='horizontal'>
+                        <PersonIcon></PersonIcon>
+                        <Typography className='contact-detail-text'>{getFullnameFromContact()}</Typography>
+                    </Layout>
+                    <Layout orientation='horizontal'>
+                        <MailOutlineIcon></MailOutlineIcon>
+                        <Typography className='contact-detail-text'>{contact?.email}</Typography>
+                    </Layout>
+                    <Layout orientation='horizontal'>
+                        <LocationOnIcon></LocationOnIcon>
+                        <Typography className='contact-detail-text'>{contact?.location}</Typography>
+                    </Layout>
+                    <Layout orientation='horizontal'>
+                        <GitHubIcon></GitHubIcon>
+                        <Typography className='contact-detail-text'>{contact?.gitHub}</Typography>
+                    </Layout>
+                    <Layout orientation='horizontal'>
+                        <LinkedInIcon></LinkedInIcon>
+                        <Typography className='contact-detail-text'>{contact?.linkedIn}</Typography>
+                    </Layout>
+                </div>
+            </LoadingSpinnerComponent>
         </div>
     )
 }
---
+++ src/main-website/employment/EmploymentTypes.tsx
@@ -0,0 +1,7 @@
+export interface IEmployment {
+    startDate: Date,
+    endDate: Date,
+    employer: string,
+    employerWebsite: string,
+    summaries: string[]
+}
