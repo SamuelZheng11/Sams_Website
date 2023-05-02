074560a18be876988821a3d22b7f51e4cd409eef
samuelzheng11@gmail.com <samuelzheng11@gmail.com>
2023-05-03 00:20:58 +1000

    Add new page to calculate settlements

--- src/settlement-calculator/slice/SettlementSlice.tsx
+++ src/settlement-calculator/slice/SettlementSlice.tsx
@@ -1,24 +1,28 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 export interface Person {
-  firstname?: string;
-  surname?: string;
+  fullName: string;
+  amount: number;
+  id: number;
+  color?: string;
+}
+
+export interface Debt {
+  creditor: Person;
+  debitor: Person;
+  debtAmount: string;
 }
 
 // Define a type for the slice state
 export interface SettlementState {
   people: Person[]
+  debts?: Debt[]
 }
 
 // Define the initial state using that type
-const initialState = {
-  people: [
-    {
-      firstname: "Sam",
-      surname: "Zheng"
-    }
-  ]
-} as SettlementState
+const initialState: SettlementState = {
+  people: [],
+}
 
 export const SettlementSlice = createSlice({
   name: 'Settlement',
@@ -30,12 +34,44 @@ export const SettlementSlice = createSlice({
         people: action.payload,
       }
     },
+    setDebts: (state, action: PayloadAction<Debt[]>) => {
+      return {
+        ...state,
+        debts: action.payload,
+      }
+    },
+    setName: (state, action: PayloadAction<{id: number, fullName: string}>) => {
+      const updatedNames = state.people.map(person => {
+        if (person.id === action.payload.id) return {...person, fullName: action.payload.fullName};
+        
+        return person;
+      })
+
+      return {
+        ...state,
+        people: updatedNames,
+      }
+    },
+    setAmount: (state, action: PayloadAction<{id: number, amount: number}>) => {
+      const updatedAmounts = state.people.map(person => {
+        if (person.id === action.payload.id) return {...person, amount: action.payload.amount};
+        return person;
+      })
+
+      return {
+        ...state,
+        people: updatedAmounts,
+      }
+    },
   }
 })
 
 // Action creators are generated for each case reducer function
 export const {
-  setPeople
+  setPeople,
+  setDebts,
+  setAmount,
+  setName,
 } = SettlementSlice.actions
 
 export default SettlementSlice.reducer
---
+++ src/utils/color.ts
@@ -0,0 +1,12 @@
+// Written by GPT
+export function getRandomHexColor(): string {
+    const hexChars = "0123456789ABCDEF";
+    let hexcode = "#";
+  
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * hexChars.length);
+      hexcode += hexChars[randomIndex];
+    }
+
+    return hexcode;
+}
