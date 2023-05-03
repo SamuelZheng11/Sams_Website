de9e89fc735d1d5449259b9a6fee1d579767e5d0
Samuel Zheng <samuelzheng11@gmail.com>
2023-05-03 18:20:50 +1000

    Fix UI bug (#31)
    * Fix Ui bug
    
    * Format settlement files using prittier

--- src/settlement-calculator/Calculator/Calculator.tsx
+++ src/settlement-calculator/Calculator/Calculator.tsx
@@ -1,32 +1,33 @@
-import { Debt, Person } from "../slice/SettlementSlice";
+import { Debt, Person } from '../slice/SettlementSlice'
 
 // Partially written by ChatGPT
 export function calculateDebts(people: Person[]) {
-  const debts: Debt[] = [];
+  const debts: Debt[] = []
 
-  if (people.length < 2) return debts;
+  if (people.length < 2) return debts
 
   for (let i = 0; i < people.length; i++) {
     for (let j = i; j < people.length; j++) {
-      if (i === j) continue;
+      if (i === j) continue
 
       // If this is 0 then neither owe each other money
-      const transaction = (people[i].amount/people.length) - (people[j].amount/people.length)
+      const transaction =
+        people[i].amount / people.length - people[j].amount / people.length
 
       if (transaction > 0) {
         // person j owes person i
         debts.push({
           debitor: people[j],
           creditor: people[i],
-          debtAmount: transaction.toFixed(2)
-        });
+          debtAmount: transaction.toFixed(2),
+        })
       } else if (transaction < 0) {
         // person i owes person j
         debts.push({
           debitor: people[i],
           creditor: people[j],
-          debtAmount: Math.abs(transaction).toFixed(2)
-        });
+          debtAmount: Math.abs(transaction).toFixed(2),
+        })
       }
     }
   }
@@ -34,16 +35,16 @@ export function calculateDebts(people: Person[]) {
   // Sort based on debitor id first then creditor id
   debts.sort((a: Debt, b: Debt) => {
     if (a.debitor.id < b.debitor.id) {
-      return -1;
+      return -1
     } else if (a.debitor.id === b.debitor.id) {
       if (a.creditor.id < b.creditor.id) {
         return -1
-      } 
+      }
 
-      return 0;
+      return 0
     }
-    return 1;
-  });
+    return 1
+  })
 
-  return debts;
+  return debts
 }
--- src/settlement-calculator/SettlementCalculatorHooks.tsx
+++ src/settlement-calculator/SettlementCalculatorHooks.tsx
@@ -1,6 +1,10 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import type { SettlementState, SettlementDispatch } from './SettlementCalculatorStore'
+import type {
+  SettlementState,
+  SettlementDispatch,
+} from './SettlementCalculatorStore'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useSettlementDispatch = () => useDispatch<SettlementDispatch>()
-export const useSettlementSelector: TypedUseSelectorHook<SettlementState> = useSelector
+export const useSettlementSelector: TypedUseSelectorHook<SettlementState> =
+  useSelector
